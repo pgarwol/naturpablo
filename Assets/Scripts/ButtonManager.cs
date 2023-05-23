e76362cdd5a3c451ab2fe1ca1bfe737ad9d9698f
Patryk Garwol <patrykgarwol2000@wp.pl>
2023-05-23 22:23:04 +0200

    basic menu

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,8 +1,22 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
+
 public class ButtonManager : MonoBehaviour {
+    // <<< MENU >>>
+    public void Play() {
+        // TODO: switch to game scene
+    }
+
+    public void ShowAuthors() {
+
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
     public void ShowAnswer() {
         // Deleting button
         Destroy(gameObject);
